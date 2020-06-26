8d779c807afd6206bb8a5afaf96870a33b856727
DanielCesarTeles <daniel.cesarteles@gmail.com>
2020-06-26 16:57:37 -0300

    First commit ExercicioVue-Js-6

---
+++ ExercicioVue-Js-6/main.js
@@ -0,0 +1,24 @@
+var vue = new Vue({
+  el:"#app",
+  data : {
+    contatos : [
+      {
+        id: 1,
+        nome: "Daniel Teles",
+        email: "abcd@gmail.com"
+      },{
+        id: 2,
+        nome: "Vitória Zanoni",
+        email: "abcd@gmail.com"
+      },{
+        id: 3,
+        nome: "Allan Teles",
+        email: "abcd@gmail.com"
+      }
+    ]
+  },
+  computed: {}
+  ,
+  methods: {}
+  }
+);
